58e96bac2c254510218e485b16a6ffd47a3e1ea0
JoieHuiju <joie.huiju@gmail.com>
2021-01-03 17:57:37 +0900

    [Update] chap14: Added react router

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,18 +1,10 @@
-import React, { useCallback, useState } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => {
-    setCategory(category);
-  }, []);
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
-  );
+  // path에 ?가 있는 이유: category 값이 선택적이기 때문. 있을 수도 있고, 없을 수도 있다.
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
--- news-viewer/src/components/Categories.js
+++ news-viewer/src/components/Categories.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 const categories = [
   {
@@ -43,7 +44,8 @@ const CategoriesBlock = styled.div`
   }
 `;
 
-const Category = styled.div`
+// 특정 컴포넌트에 styled-components를 사용할 때는 styled(컴포넌트 이름)``과 같은 형식을 사용한다.
+const Category = styled(NavLink)`
   font-size: 1.125rem;
   cursor: pointer;
   white-space: pre;
@@ -55,30 +57,32 @@ const Category = styled.div`
     color: #495057;
   }
 
-  ${(props) =>
-    props.active &&
-    css`
-      font-weight: 600;
-      border-bottom: 2px solid #22b8cf;
-      color: #22b8cf;
-      &:hover {
-        color: #3bc9db;
-      }
-    `}
+  &:active {
+    font-weight: 600;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
+    &:hover {
+      color: #3bc9db;
+    }
+  }
 
   & + & {
     margin-left: 1rem;
   }
 `;
 
-const Categories = ({ category, onSelect }) => {
+const Categories = () => {
+  // 전체보기의 경우 /all 대신에 /로 설정했음
+  // to 값이 /을 가리키고 있을 때, exact 값을 true로 해 주어야 함
+  // 이 값을 설정하지 않으면, 다른 카테고리가 선택되었을 때도 전체보기 링크에 active 스타일이 적용됨
   return (
     <CategoriesBlock>
       {categories.map((c) => (
         <Category
           key={c.name}
-          active={category === c.name}
-          onClick={() => onSelect(c.name)}
+          activeClassName="active"
+          exact={c.name === 'all'}
+          to={c.name === 'all' ? '/' : `/${c.name}`}
         >
           {c.text}
         </Category>
